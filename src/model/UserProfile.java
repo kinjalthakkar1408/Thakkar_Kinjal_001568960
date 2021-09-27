2a28ee63c9961b2497dc255d0656ab251cdef39b
Kinjal Thakkar <thakkar.k@northeastern.edu>
2021-09-27 09:35:07 +0530

    committing AED assignment 1

---
+++ src/model/UserProfile.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Kinjal Thakkar
+ */
+public class UserProfile {
+    
+    private String name;
+    private String geographicalData;
+    private String dateOfBirth;
+    private long teleNumber;
+    private long faxNum;
+    private String emailID;
+    private long ssn;
+    private long medRecordNum;
+    private long healthPlanBeneficiaryNum;
+    private long bankAccountNum;
+    private long certLicenseNum;
+    private String licensePlate;
+    private String devIdentifier;
+    private String linkedIn;
+    private String ipAdress;
+    private ImageIcon image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicalData() {
+        return geographicalData;
+    }
+
+    public void setGeographicalData(String geographicalData) {
+        this.geographicalData = geographicalData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(long teleNumber) {
+        this.teleNumber = teleNumber;
+    }
+
+    public long getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(long faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public long getMedRecordNum() {
+        return medRecordNum;
+    }
+
+    public void setMedRecordNum(long medRecordNum) {
+        this.medRecordNum = medRecordNum;
+    }
+
+    public long getHealthPlanBeneficiaryNum() {
+        return healthPlanBeneficiaryNum;
+    }
+
+    public void setHealthPlanBeneficiaryNum(long healthPlanBeneficiaryNum) {
+        this.healthPlanBeneficiaryNum = healthPlanBeneficiaryNum;
+    }
+
+    public long getBankAccountNum() {
+        return bankAccountNum;
+    }
+
+    public void setBankAccountNum(long bankAccountNum) {
+        this.bankAccountNum = bankAccountNum;
+    }
+
+    public long getCertLicenseNum() {
+        return certLicenseNum;
+    }
+
+    public void setCertLicenseNum(long certLicenseNum) {
+        this.certLicenseNum = certLicenseNum;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getDevIdentifier() {
+        return devIdentifier;
+    }
+
+    public void setDevIdentifier(String devIdentifier) {
+        this.devIdentifier = devIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAdress() {
+        return ipAdress;
+    }
+
+    public void setIpAdress(String ipAdress) {
+        this.ipAdress = ipAdress;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    
+}
